a712fd5240d6fc95d9f2ce9c60b1109a766b0d37
My-Gamez <116650818+My-Gamez@users.noreply.github.com>
2022-10-26 14:40:18 +0530

    Update index.js

--- TypeMaster/index.js
+++ TypeMaster/index.js
@@ -27,7 +27,7 @@ document.onkeydown = function (e) {
             document.getElementById('check').style.borderStyle="inset";
             document.getElementById('check').style.borderColor="#006400";
             setTimeout(() => {
-                window.location.href="http://127.0.0.1:5500/index.html";
+                window.location.href="https://My-Gamez.github.io/file/TypeMaster";
             }, 2000);
             let element=document.getElementById('check');
             element.classList.add('checkani');
