f896d81a0a4fa0274874170657eb5a9605419f41
My-Gamez <116650818+My-Gamez@users.noreply.github.com>
2022-12-07 11:02:31 +0530

    Update script.js

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -176,5 +176,5 @@ function check(){
     }
 }
 function replay(){
-    window.location.href = "http://my-gamez.github.io/file/Tic Tac Toe/";
+    window.location.href = "http://my-gamez.github.io/HTML-CSS-JS/Tic Tac Toe/";
 }
